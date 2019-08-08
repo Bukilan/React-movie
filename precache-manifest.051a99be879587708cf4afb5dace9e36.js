23cad4963d9bd7a1c048d7ef84884c8af33a868f
Golod Egor <egor.golod.firefly@gmail.com>
2019-08-09 01:16:43 +0300

    Updates

---
+++ precache-manifest.051a99be879587708cf4afb5dace9e36.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "7cb3dd8036e0108e994061441da1fc09",
+    "url": "/mvdb-api-react-build/static/media/image2vector.7cb3dd80.svg"
+  },
+  {
+    "revision": "88823c2015ffd5fa89d567e17297a137",
+    "url": "/mvdb-api-react-build/static/media/Roboto-Light.88823c20.ttf"
+  },
+  {
+    "revision": "41994d44eb6ccdecd639",
+    "url": "/mvdb-api-react-build/static/js/runtime~main.d0363bd3.js"
+  },
+  {
+    "revision": "ce4ee65ca5f5d98ad230",
+    "url": "/mvdb-api-react-build/static/js/main.9d831651.chunk.js"
+  },
+  {
+    "revision": "ddda13c9dbdedb510b55",
+    "url": "/mvdb-api-react-build/static/js/2.af3d4b8c.chunk.js"
+  },
+  {
+    "revision": "ce4ee65ca5f5d98ad230",
+    "url": "/mvdb-api-react-build/static/css/main.1ab36781.chunk.css"
+  },
+  {
+    "revision": "77365e33903fbfd7874a90ff7aaeaabc",
+    "url": "/mvdb-api-react-build/index.html"
+  }
+];
